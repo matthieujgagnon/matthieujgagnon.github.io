ff2e125cc1c76c435ac2acb0d5d5a7b8a32b982a
“matthieujgagnon” <matthieugagnon@me.com>
2024-10-29 00:34:13 -0400

    Barebones

--- js/main.js
+++ js/main.js
@@ -2,16 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.getElementById('hamburger');
     const navMenu = document.getElementById('nav-menu');
 
-    // Toggle menu on mobile
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('active');
         navMenu.classList.toggle('active');
-    });
-
-    // Ensure menu is always visible on desktop
-    window.addEventListener('resize', () => {
-        if (window.innerWidth >= 768) {
-            navMenu.classList.remove('active'); // Always visible on desktop
-        }
     });
 });
